dc367ab5e97b4aeac3891c8e9c0836a502bf0e04
JaeWon Baek <bjw1622@gmail.com>
2022-12-07 14:53:32 +0900

    add boardIndex getServerSideProps

--- pages/board/index.js
+++ pages/board/index.js
@@ -1,2 +1,23 @@
-const boardIndex = () => {};
-export default boardIndex;
+import axios from "axios";
+
+const BoardIndex = ({ data }) => {
+  console.log({ data });
+  return (
+    <div>
+      <a href=""></a>
+    </div>
+  );
+};
+export const getServerSideProps = async () => {
+  try {
+    const res = await axios.get(
+      `http://board-www.board.com/Board/IndexBoardList`
+    );
+    const data = await res.data;
+    console.log(data);
+    return { props: { data } };
+  } catch (error) {
+    return { props: {} };
+  }
+};
+export default BoardIndex;
