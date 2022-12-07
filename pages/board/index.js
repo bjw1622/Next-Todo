962e269535cde6e9c4987af6f1e2e2910433911c
JaeWon Baek <bjw1622@gmail.com>
2022-12-07 15:21:01 +0900

    boardLayout 추가

--- pages/board/index.js
+++ pages/board/index.js
@@ -1,8 +1,90 @@
 import axios from "axios";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Table from "react-bootstrap/Table";
+import Form from "react-bootstrap/Form";
+import BoardItem from "../../component/layout/board/BoardItem";
 const BoardIndex = ({ data }) => {
-  console.log({ data });
-  return;
+  console.log({ data }.data);
+  return (
+    <div>
+      <div>
+        <Form.Select
+          aria-label="Default select example"
+          style={{ width: "75px", height: "40px", marginRight: "5px" }}
+        >
+          <option value="20">20</option>
+          <option value="30">30</option>
+          <option value="40">40</option>
+          <option value="50">50</option>
+        </Form.Select>
+        <Form.Select
+          aria-label="Default select example"
+          style={{ width: "100px", height: "40px", marginRight: "5px" }}
+        >
+          <option value="Name">작성자</option>
+          <option value="Title">제목</option>
+        </Form.Select>
+        <Form>
+          <Form.Group
+            className="mb-3"
+            controlId="exampleForm.ControlInput1"
+            style={{ width: "200px", height: "45px", marginRight: "5px" }}
+          >
+            <Form.Control type="text" style={{ height: "40px" }} />
+          </Form.Group>
+        </Form>
+        <Button
+          variant="info"
+          style={{ width: "60px", height: "40px", color: "white" }}
+        >
+          검색
+        </Button>
+      </div>
+      <Table striped hover>
+        <thead style={{ borderStyle: "none" }}>
+          <tr>
+            <th className="text-center">#</th>
+            <th className="text-center">제목</th>
+            <th className="text-center">작성자</th>
+            <th className="text-center">댓글</th>
+            <th className="text-center">추천</th>
+          </tr>
+        </thead>
+        <tbody>
+          {{ data }.data.map((item, num) => {
+            return (
+              <BoardItem
+                key={item.No}
+                number={num + 1}
+                boardinfo={item}
+              ></BoardItem>
+            );
+          })}
+        </tbody>
+      </Table>
+      <ButtonGroup
+        className="me-2"
+        aria-label="First group"
+        style={{ display: "block", textAlign: "center" }}
+      >
+        <Button variant="secondary" style={{ color: "white" }}>
+          1
+        </Button>
+        <Button variant="secondary" style={{ color: "white" }}>
+          2
+        </Button>
+        <Button variant="secondary" style={{ color: "white" }}>
+          3
+        </Button>
+        <Button variant="secondary" style={{ color: "white" }}>
+          4
+        </Button>
+      </ButtonGroup>
+      <Button variant="primary">글쓰기</Button>
+    </div>
+  );
 };
 
 export const getServerSideProps = async () => {
