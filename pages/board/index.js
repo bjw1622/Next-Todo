484ca9dda262af51fdb9a4fa438772d8ee314355
JaeWon Baek <bjw1622@gmail.com>
2022-12-07 15:08:21 +0900

    getServerSidePorps boardlist data

--- pages/board/index.js
+++ pages/board/index.js
@@ -2,22 +2,19 @@ import axios from "axios";
 
 const BoardIndex = ({ data }) => {
   console.log({ data });
-  return (
-    <div>
-      <a href=""></a>
-    </div>
-  );
+  return;
 };
+
 export const getServerSideProps = async () => {
   try {
     const res = await axios.get(
       `http://board-www.board.com/Board/IndexBoardList`
     );
     const data = await res.data;
-    console.log(data);
     return { props: { data } };
   } catch (error) {
     return { props: {} };
   }
 };
+
 export default BoardIndex;
