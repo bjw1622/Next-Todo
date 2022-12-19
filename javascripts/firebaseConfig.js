86428c5dbabceb604772eb8073e82d7fdc4a331d
JaeWon Baek <bjw1622@gmail.com>
2022-12-19 10:02:23 +0900

    firebase initailize error 수정

--- javascripts/firebaseConfig.js
+++ javascripts/firebaseConfig.js
@@ -1,24 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth } from "firebase/auth";
-// Your web app's Firebase configuration
-// Import the functions you need from the SDKs you need
-
-// Your web app's Firebase configuration
+import firebase from "firebase/compat/app";
 const firebaseConfig = {
   apiKey: "AIzaSyBc6sMf-h5sd8N9TuF_2M3s8xOhLPtof4w",
   authDomain: "nextauth-pratice.firebaseapp.com",
+  // databaseURL: "https://test.firebaseio.com",
   projectId: "nextauth-pratice",
   storageBucket: "nextauth-pratice.appspot.com",
   messagingSenderId: "313275721428",
   appId: "1:313275721428:web:473155ed281474553bbd41",
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-// Initialize Firebase
-export const fbApp = initializeApp(firebaseConfig);
-export const fbAuth = getAuth(fbApp);
+const fbase = !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+export const fbAuth = getAuth(fbase);
