0b2d4995381a63b9ee67c1c1a79163fd731bd300
JaeWon Baek <bjw1622@gmail.com>
2022-12-21 17:24:05 +0900

    accessToken 수정🎉

--- pages/board/accessToken.js
+++ pages/board/accessToken.js
@@ -1,6 +1,10 @@
-import { useSession, signIn, signOut } from "next-auth/react";
-export default function Component() {
-  const { data } = useSession();
-  const { accessToken } = data;
-  return <div>Access Token: {accessToken}</div>;
-}
+// import { useSession, signIn, signOut } from "next-auth/react";
+// export default function Component() {
+//   const { data } = useSession();
+//   const { accessToken } = data;
+//   return <div>Access Token: {accessToken}</div>;
+// }
+const accessToken = () => {
+  return <div></div>;
+};
+export default accessToken;
