5adc04ab6b787a6c8923e1fe442f8f46171856f1
JaeWon Baek <bjw1622@gmail.com>
2023-01-03 10:32:38 +0900

    next auth secret 값 추가

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import "../styles/globals.scss";
 import { SessionProvider } from "next-auth/react";
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <SessionProvider session={session} basePath="/api/auth">
+    <SessionProvider session={session}>
       <BaseLayout>
         <Component {...pageProps} />
       </BaseLayout>
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -51,5 +51,5 @@ export default NextAuth({
       }
     },
   },
-  secret: process.env.SECRET,
+  secret: "bKhMI6aa5nPGR2yj4UfJ8rbO8e2lEq3IOdwn8yGQ7nw=",
 });
