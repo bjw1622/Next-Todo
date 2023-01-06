89cac0e918340e3961e3d7b8c7d35d4c410508b2
JaeWon Baek <bjw1622@gmail.com>
2023-01-06 09:44:03 +0900

    next api 통합

--- pages/api/todo.js
+++ pages/api/todo.js
@@ -1,36 +1,57 @@
 import { db } from "../../javascripts/firebaseConfig";
-import { addDoc, collection, getDocs } from "firebase/firestore";
-import { useSession } from "next-auth/react";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  updateDoc,
+} from "firebase/firestore";
 
 export default async function Todo(req, res) {
-  const todoListCollectionCollectionRef = collection(db, "todo");
-  const { data } = useSession();
-  if (data === null) {
-    console.log("test");
-    return;
-  }
-  if (req.method === "POST") {
+  if (req.method === "GET") {
     try {
-      await addDoc(todoListCollectionCollectionRef, {
-        inputValue: req.body.inputValue,
-        check: req.body.check,
-      });
-      // 값 전달 true or false
-      // api 하나로 합치기
-      // catch return
-      res.status(200).json(req.body);
-    } catch (error) {
-      console.log(error);
-    }
-  } else if (req.method === "GET") {
-    try {
-      const querySnapshot = await getDocs(collection(db, "todo"));
+      console.log(req.query);
+      // emaildata 정보 어떻게??
+      const querySnapshot = await getDocs(collection(db, req.query.emailData));
       // data 유효성체크 null
       const data = [];
       querySnapshot.forEach((doc) => {
         data.push({ ...doc.data(), id: doc.id });
       });
       res.status(200).json(data);
+    } catch (error) {
+      console.log(error);
+    }
+  } else if (req.method === "POST") {
+    try {
+      const todoListCollectionCollectionRef = collection(
+        db,
+        req.body.emailData
+      );
+      await addDoc(todoListCollectionCollectionRef, {
+        inputValue: req.body.inputValue,
+        check: req.body.check,
+      });
+      // 값 전달 true or false
+      // catch return
+      res.status(200).json(req.body);
+    } catch (error) {
+      console.log(error);
+    }
+  } else if (req.method === "DELETE") {
+    try {
+      const todoDoc = doc(db, req.body.emailData, req.body.id);
+      await deleteDoc(todoDoc);
+      res.status(200).json({ data: todoDoc });
+    } catch (error) {
+      console.log(error);
+    }
+  } else if (req.method === "PUT") {
+    try {
+      const todoDoc = doc(db, req.body.emailData, req.body.id);
+      await updateDoc(todoDoc, { inputValue: req.body.inputValue });
+      res.status(200).json(req.body);
     } catch (error) {
       console.log(error);
     }
