a96492aef79ff9b6178433b916f73d5f237d7dc9
JaeWon Baek <bjw1622@gmail.com>
2023-01-09 14:19:54 +0900

    profile이미지 변경 추가🎉

--- component/layout/baseLayout.js
+++ component/layout/baseLayout.js
@@ -1,11 +1,21 @@
+import { getDownloadURL, getStorage, listAll, ref } from "firebase/storage";
 import { useSession, signIn, signOut } from "next-auth/react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import styles from "../../styles/baseLayout.module.scss";
+import firebaseApp from "../../javascripts/firebaseConfig";
+import { useState } from "react";
+
 const BaseLayout = ({ children }) => {
   const router = useRouter();
+  const [profileImg, setProfileImg] = useState("");
   const { data } = useSession();
   if (data) {
+    const storage = getStorage(firebaseApp);
+    const imageRef = ref(storage, `images/${data.user.email}`);
+    getDownloadURL(ref(imageRef)).then((url) => {
+      setProfileImg(url);
+    });
     return (
       <>
         <div className={styles.baseLayout}>
@@ -25,16 +35,20 @@ const BaseLayout = ({ children }) => {
               <h4>Todo</h4>
             </Link>
           </li>
-          <li className={styles.baseLi}>
-            <Link
-              href={"/imageBtn"}
-              className={router.pathname.includes("/imageBtn") ? "active" : ""}
-            >
-              <h4>imageBtn</h4>
+          <div className={styles.logIn}>
+            <Link href={"/imageBtn"}>
+              <img
+                className={styles.profileImg}
+                id="profileImg"
+                src={profileImg}
+              />
             </Link>
-          </li>
-          <div>
-            {data.user.name}님 환영합니다
+            <span>
+              <Link href={"/imageBtn"}>
+                <h4 id={styles.profileId}>{data.user.name}</h4>
+              </Link>
+              님 환영합니다
+            </span>
             <button onClick={() => signOut()}>Sign out</button>
           </div>
         </div>
@@ -56,14 +70,6 @@ const BaseLayout = ({ children }) => {
             className={router.pathname === "/todo" ? "active" : ""}
           >
             <h4>Todo</h4>
-          </Link>
-        </li>
-        <li className={styles.baseLi}>
-          <Link
-            href={"/imageBtn"}
-            className={router.pathname.includes("/imageBtn") ? "active" : ""}
-          >
-            <h4>imageBtn</h4>
           </Link>
         </li>
         <div>
