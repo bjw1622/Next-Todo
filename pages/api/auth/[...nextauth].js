d5b98ee7b7959b66221efd628e880601597ce324
JaeWon Baek <bjw1622@gmail.com>
2023-01-09 17:35:01 +0900

    nextauth provider 수정

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,19 +7,19 @@ import { fbAuth } from "../../../javascripts/firebaseConfig";
 
 export default NextAuth({
   providers: [
-    GithubProvider({
-      clientId: "257ac962cea6392c09df",
-      clientSecret: "ab8b1a5ed78eaac8aa188deffd3d70c40c11298f",
-    }),
+    // GithubProvider({
+    //   clientId: "257ac962cea6392c09df",
+    //   clientSecret: "ab8b1a5ed78eaac8aa188deffd3d70c40c11298f",
+    // }),
     GoogleProvider({
       clientId:
         "413999012118-t9ln1oq4rprvgglctre1unks9j285k3u.apps.googleusercontent.com",
       clientSecret: "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",
     }),
-    KakaoProvider({
-      clientId: "1166cc6dd524d73516b8eb8671f1bcb0",
-      clientSecret: "5acgT51YvTUGi2x9DxyCzAvQyusZPa4o",
-    }),
+    // KakaoProvider({
+    //   clientId: "1166cc6dd524d73516b8eb8671f1bcb0",
+    //   clientSecret: "5acgT51YvTUGi2x9DxyCzAvQyusZPa4o",
+    // }),
   ],
   callbacks: {
     async jwt({ token, account }) {
