dfaf0f480d020ff610dcc947bf9f6cd47db8368e
JaeWon Baek <bjw1622@gmail.com>
2023-01-18 13:36:43 +0900

    nextauth callback 수정

--- next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  env: {
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    GOOGLE_ID: process.env.GOOGLE_ID,
-    GOOGLE_SECRET: process.env.GOOGLE_SECRET,
-  },
-};
