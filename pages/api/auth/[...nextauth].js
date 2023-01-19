33d946bfc5ca7b9e12c3c4155945e3ff37363f33
JaeWon Baek <bjw1622@gmail.com>
2023-01-19 10:05:41 +0900

    kakaoprovider 삭제

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,6 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
-import KakaoProvider from "next-auth/providers/kakao";
 export const authOptions = {
   providers: [
     GithubProvider({
@@ -12,10 +11,6 @@ export const authOptions = {
       clientId:
         "413999012118-t9ln1oq4rprvgglctre1unks9j285k3u.apps.googleusercontent.com",
       clientSecret: "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",
-    }),
-    KakaoProvider({
-      clientId: "1166cc6dd524d73516b8eb8671f1bcb0",
-      clientSecret: "5acgT51YvTUGi2x9DxyCzAvQyusZPa4o",
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
