14fe54f34067a996c79f34802f661cd2aad487b3
JaeWon Baek <bjw1622@gmail.com>
2023-01-20 11:20:18 +0900

    Layout loading spinner 추가

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,30 @@
 import BaseLayout from "../component/layout/baseLayout";
 import "../styles/globals.scss";
 import { SessionProvider } from "next-auth/react";
-function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+import Router from "next/router";
+import Loading from "../component/common/Loading";
+import { useEffect, useState } from "react";
+
+const MyApp = ({ Component, pageProps: { session, ...pageProps } }) => {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const start = () => {
+      setLoading(true);
+    };
+    const end = () => {
+      setLoading(false);
+    };
+    Router.events.on("routeChangeStart", start);
+    Router.events.on("routeChangeComplete", end);
+  }, []);
+
   return (
     <SessionProvider session={session} basePath="/api/auth">
       <BaseLayout>
-        <Component {...pageProps} />
+        {loading ? <Loading></Loading> : <Component {...pageProps} />}
       </BaseLayout>
     </SessionProvider>
   );
-}
-
+};
 export default MyApp;
